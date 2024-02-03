5beadfd418599e08e1a60d30becf2cc5b15f6481
Omar Ashraf <58981064+omarashrafhamdy@users.noreply.github.com>
2024-02-03 16:03:36 +0200

    finished

--- Bash Scripts/database_backup.sh
+++ Bash Scripts/database_backup.sh
@@ -4,6 +4,8 @@ DB_USER=University
 DB_PASSWORD=123
 DB_SID=XE
 
+BACKUP_DIR="/d/ITI/Case Study/Bash Scripts/Backup"
+
 DATE_FORMAT=$(date +"%Y%m%d_%H%M%S")
 
 EXPORT_FILE="backup_${DATE_FORMAT}.dmp"
@@ -11,9 +13,8 @@ EXPORT_FILE="backup_${DATE_FORMAT}.dmp"
 expdp ${DB_USER}/${DB_PASSWORD}@${DB_SID} DIRECTORY=DATA_PUMP_DIR DUMPFILE=${EXPORT_FILE} FULL=Y
 
 if [ $? -eq 0 ]; then
-    echo "Database backup successful. File: ${EXPORT_FILE}"
+    cp "/c/oraclexe/app/oracle/admin/XE/dpdump/${EXPORT_FILE}" "${BACKUP_DIR}/"
+    echo "Database backup successful. File: ${EXPORT_FILE} | date: $(date)" >> "${BACKUP_DIR}/backuplogs.txt"
 else
-    echo "Error: Database backup failed."
+    echo "Error: Database backup failed. | date: $(date)" >> "${BACKUP_DIR}/backuplogs.txt"
 fi
-
-
--- Bash Scripts/disk_space.sh
+++ Bash Scripts/disk_space.sh
@@ -8,7 +8,7 @@ disk_space=`df -h | awk ' NR==2 {print $6}' | sed 's/%//'`
 echo $disk_space
 
 if [[ $disk_space -gt $threshold ]]; then
-	echo "Warning Disk Space Is Above $threshold%" >> /D/ITI/Case\ Study/Bash\ Scripts/disk_log.txt
+	echo "Warning Disk Space Is Above $threshold% | date: $(date)" >> /D/ITI/Case\ Study/Bash\ Scripts/disk_log.txt
 else
-	echo "Disk Space Is Under $threshold%" >> /D/ITI/Case\ Study/Bash\ Scripts/disk_log.txt
+	echo "Disk Space Is Under $threshold% | date: $(date)" >> /D/ITI/Case\ Study/Bash\ Scripts/disk_log.txt
 fi
